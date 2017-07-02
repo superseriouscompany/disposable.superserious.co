d643d8743e434c7289c67f05060ed60028531b1f
Neil Sarkar <neil.r.sarkar@gmail.com>
2017-07-02 08:56:50 -0400

    Return 201 instead of 200

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@ const models = {
 module.exports = function(app) {
   app.post('/users', function(req, res, next) {
     models.user.create(req.body).then((u) => {
-      res.json(u)
+      res.status(201).json(u)
     }).catch((err) => {
       if( err.message.match(/^ValidationError/) ) {
         return res.status(400).json({message: err.message})
